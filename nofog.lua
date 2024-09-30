30222f1ed4bf1628430b73301c8fe4ffafb8cefa
lampenkop <pepijnvdtuin@gmail.com>
2024-09-30 16:13:58 +0200

    Create nofog.lua

---
+++ nofog.lua
@@ -0,0 +1,8 @@
+-- Get the Lighting service
+local Lighting = game:GetService("Lighting")
+
+-- Check if there is a Sky instance and delete it
+local skyInstance = Lighting:FindFirstChildOfClass("Sky")
+if skyInstance then
+    skyInstance:Destroy()
+end
