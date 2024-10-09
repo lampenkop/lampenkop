8fc2fefefdb52778b4663f7ea566db4e2ead295e
lampenkop <pepijnvdtuin@gmail.com>
2024-10-09 20:18:34 +0200

    Update gui2.lua

--- gui2.lua
+++ gui2.lua
@@ -12,14 +12,15 @@ local Window = Rayfield:CreateWindow({
    Discord = {
       Enabled = false,
       Invite = "noinvite", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
    },
+   
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
       Title = "Nigger hub",
       Subtitle = "Key System",
       Note = "Key In Discord Server",
-      FileName = "YoutubeHubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      FileName = "Sigma", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"https://pastebin.com/raw/6XLmMtv7"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
